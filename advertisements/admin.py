0a6d7c81cb37461793dc565c25ae82569c8db0ae
Lee <84514310+Lee-Rose@users.noreply.github.com>
2022-03-17 14:49:55 +0200

    completed main part/

--- advertisements/admin.py
+++ advertisements/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+#unregister the provided model admin
+admin.site.unregister(User)
+
+
+#register out own model admin, based on the default UserAdmin
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    actions = ['update', 'destroy',]
+
+
